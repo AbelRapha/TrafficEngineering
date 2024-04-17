88fa24953ddf8499dda6ce23b212cc009d42803d
Abel <79184789+AbelRapha@users.noreply.github.com>
2024-04-17 17:13:27 +0000

    Create Audiobook and Chatbot

---
+++ app.py
@@ -0,0 +1,60 @@
+from rag_config import invoke_chain
+import streamlit as st 
+from langchain_core.messages import AIMessage, HumanMessage
+
+st.set_page_config(page_title = 'Bot EngTrafego', page_icon = '👷‍♂️')
+
+st.title('Bot EngTráfego 👷‍♂️')
+
+st.write('## Clique no player abaixo para ouvir o áudio livro completo')
+
+audio_file = open("ingest/docs/audiobook.mp3", "rb")
+audio_bytes = audio_file.read()
+
+st.audio(audio_bytes, format="audio/mp3")
+
+st.write("## ChatBot :robot_face:")
+st.info('Escreva sua pergunta abaixo e converse com o capítulo 2 sobre Polos Geradores de Tráfego')
+
+# query = st.text_input('Pergunta', 'Ex: Faça um resumo sobre o capítulo inteiro de forma coesa utilizando até 5 mil palavras')
+# if st.button('Gerar Resposta'):
+#     with st.spinner('Gerando resposta, aguarde...'):
+#         response = invoke_chain(query)
+#         if response:
+#             st.write(response)
+
+# session state
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Olá, como posso ajudar?"),
+    ]
+
+    
+# conversation
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
+# user input
+user_query = st.chat_input("Digite sua pergunta aqui...")
+if user_query is not None and user_query != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+    with st.chat_message("AI"):
+        response = invoke_chain(user_query)
+        st.write_stream(response)
+
+    st.session_state.chat_history.append(AIMessage(content=response))
+
+# Button to clear chat history
+
+if st.button('Limpar histórico'):
+    del st.session_state.chat_history
+    st.rerun()
---
+++ rag_config.py
@@ -0,0 +1,43 @@
+from langchain_pinecone import PineconeVectorStore
+from langchain_google_genai import ChatGoogleGenerativeAI , GoogleGenerativeAIEmbeddings
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import PromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Config Vector DB and retriver
+
+index_name = "trafficengineering"
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+docsearch = PineconeVectorStore(embedding = embeddings, index_name=index_name)
+
+retriever = docsearch.as_retriever(search_type="mmr")
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Prompt Template
+
+llm_prompt_template = """Você agora é um assistente que responderá perguntas sobre o capítulo de um livro relacionado a engenharia de tráfego.
+Sendo assim, apenas utilize do contexto para se basear e criar uma resposta coesa. 
+Sempre responda no idioma Portguês do Brasil.\n
+Contexto: {context} \nPergunta: {question} \nResposta:"""
+
+llm_prompt = PromptTemplate.from_template(llm_prompt_template)
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5, top_p=1)
+    
+rag_chain = (
+    {"context": retriever | format_docs ,"question": RunnablePassthrough()}
+    | llm_prompt
+    | llm
+    | StrOutputParser()
+)
+
+def invoke_chain(query):
+    return rag_chain.stream(query)
